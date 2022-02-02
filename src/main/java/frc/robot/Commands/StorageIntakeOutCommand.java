889c40ed474f8e4cf8ae3393be8ad6158b8a3bf3
Harmiess <alexanderstamper@gmail.com>
2022-02-01 18:37:23 -0600

    uploading missing files

---
+++ src/main/java/frc/robot/Commands/StorageIntakeOutCommand.java
@@ -0,0 +1,10 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import frc.robot.Subsystems.Swerve.StorageIntake;
+
+public class StorageIntakeOutCommand extends StartEndCommand {
+    public StorageIntakeOutCommand(StorageIntake subsystem) {
+        super(subsystem::storageIntakeOut, subsystem::storageIntakeStop);
+    }
+}
