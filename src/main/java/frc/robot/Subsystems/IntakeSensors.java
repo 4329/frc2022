25dca982ed4ba5699904cdf67ca4fc0ec50617e2
Harmiess <alexanderstamper@gmail.com>
2022-02-07 19:18:42 -0600

    added shuffleboard info (appears in Competition Info tab)

--- src/main/java/frc/robot/Subsystems/IntakeSensors.java
+++ src/main/java/frc/robot/Subsystems/IntakeSensors.java
@@ -1,19 +1,32 @@
 package frc.robot.Subsystems;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import frc.robot.Configrun;
 
 public class IntakeSensors {
-    //upper intake sensor
-    DigitalInput topTrigger = new DigitalInput(Configrun.get(1,"shooterFeedTriggerPort"));
+    private NetworkTableEntry topTriggerStatus;
+    private NetworkTableEntry bottomTriggerStatus;
 
-    public boolean topTrigger() {
-        return topTrigger.get();
-    }
-    //lower intake sensor
+    DigitalInput topTrigger = new DigitalInput(Configrun.get(1,"shooterFeedTriggerPort"));
     DigitalInput bottomTrigger = new DigitalInput(Configrun.get(3,"storageIntakeTriggerPort"));
 
+    public IntakeSensors() {
+        topTriggerStatus = Shuffleboard.getTab("Competition Info").add("Top Trigger",topTrigger()).withWidget(BuiltInWidgets.kBooleanBox).getEntry();
+        bottomTriggerStatus = Shuffleboard.getTab("Competition Info").add("Bottom Trigger",bottomTrigger()).withPosition(0,1).withWidget(BuiltInWidgets.kBooleanBox).getEntry();
+    }
+
+    public boolean topTrigger() {
+        topTriggerStatus.setBoolean(topTrigger.get());
+
+        return topTrigger.get();
+    }
+
     public boolean bottomTrigger() {
+        bottomTriggerStatus.setBoolean(bottomTrigger.get());
+
         return bottomTrigger.get();
     }
 }
