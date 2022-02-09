0be71fe78d750de20319df73a20474050cb69235
MrUnderAverage <benjaminjdurbin@gmail.com>
2022-02-08 20:31:00 -0600

    Implemented Camera streams on shuffleboard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,7 +3,11 @@ package frc.robot;
 import frc.robot.Subsystems.Swerve.*;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.*;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.HttpCamera;
+import edu.wpi.first.cscore.VideoSource;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -24,7 +28,6 @@ import frc.robot.Commands.IntakeSensorsCommand;
 import frc.robot.Commands.IntakeSolenoidDownCommand;
 import frc.robot.Commands.StorageIntakeInCommand;
 import frc.robot.Commands.StorageIntakeOutCommand;
-import frc.robot.Commands.Autos.MoveOneMeterAuto;
 import frc.robot.Constants.*;
 import frc.robot.Subsystems.IntakeSensors;
 import frc.robot.Subsystems.ShooterFeedSubsytem;
@@ -40,19 +43,15 @@ import edu.wpi.first.wpilibj.PneumaticHub;
  */
 public class RobotContainer {
 
-private static final int PH_CAN_ID = Configrun.get(61, "PH_CAN_ID");
-private PneumaticHub pneumaticHub = new PneumaticHub(PH_CAN_ID);;
+  private static final int PH_CAN_ID = Configrun.get(61, "PH_CAN_ID");
+  private PneumaticHub pneumaticHub = new PneumaticHub(PH_CAN_ID);;
 
-private IntakeMotor intakeMotor = new IntakeMotor();
-private IntakeSolenoidSubsystem intakeSolenoid = new IntakeSolenoidSubsystem(pneumaticHub);
+  private IntakeMotor intakeMotor = new IntakeMotor();
+  private IntakeSolenoidSubsystem intakeSolenoid = new IntakeSolenoidSubsystem(pneumaticHub);
 
-
-
-ParallelCommandGroup intakeCommandGroup() {
-  return new ParallelCommandGroup(new IntakeSolenoidDownCommand(intakeSolenoid), new IntakeRunCommand(intakeMotor));
-}
-
-
+  ParallelCommandGroup intakeCommandGroup() {
+    return new ParallelCommandGroup(new IntakeSolenoidDownCommand(intakeSolenoid), new IntakeRunCommand(intakeMotor));
+  }
 
   // The robot's subsystems
   private final Drivetrain m_robotDrive;
@@ -72,10 +71,11 @@ ParallelCommandGroup intakeCommandGroup() {
   private final Command moveOneMeter;
   private final Command twoPaths;
   private final Command intakeRun;
-  //private final Command exampleAuto;
+  // private final Command exampleAuto;
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
+   *
    * @param drivetrain
    */
   public RobotContainer(Drivetrain drivetrain) {
@@ -83,14 +83,35 @@ ParallelCommandGroup intakeCommandGroup() {
     moveOneMeter = new MoveOneMeterAuto(m_robotDrive);
     twoPaths = new TwoPathsAuto(m_robotDrive);
     intakeRun = new IntakeRunAuto(m_robotDrive);
-    //exampleAuto = new ExampleAuto(m_robotDrive);
+    // exampleAuto = new ExampleAuto(m_robotDrive);
     m_drive = new DriveByController(m_robotDrive, m_driverController);
+
+    initializeCamera();
 
     configureAutoChooser();
     configureButtonBindings(); // Configure the button bindings to commands using configureButtonBindings
                                // function
     // Configure default commands
     m_robotDrive.setDefaultCommand(m_drive); // Set drivetrain default command to "DriveByController"
+  }
+
+  // Creates and establishes camera streams for the shuffleboard ~Ben
+  private void initializeCamera() {
+    CameraServer.startAutomaticCapture();
+    VideoSource[] enumerateSources = VideoSource.enumerateSources();
+
+    System.out.println("            Number of Sources:" + enumerateSources.length);
+    System.out.println("            Name of Source" + enumerateSources[0].getName());
+
+    if (enumerateSources.length > 0 && enumerateSources[0].getName().contains("USB")) {
+      Shuffleboard.getTab("Camera Test Tab").add("Camera 1", enumerateSources[0]).withPosition(0, 0).withSize(4, 4)
+          .withWidget(BuiltInWidgets.kCameraStream);
+    }
+    HttpCamera limelight = new HttpCamera("Limelight", "http://10.43.29.11:5800");
+    CameraServer.startAutomaticCapture(limelight);
+
+    Shuffleboard.getTab("Camera Test Tab").add("Limelight Baby", limelight).withPosition(4, 0).withSize(2, 2)
+        .withWidget(BuiltInWidgets.kCameraStream);
   }
 
   /**
@@ -110,27 +131,34 @@ ParallelCommandGroup intakeCommandGroup() {
     new JoystickButton(m_driverController, Button.kRightBumper.value).whenPressed(() -> m_drive.changeFieldOrient());
 
     new JoystickButton(m_operatorController, Button.kY.value).whileHeld(new ShooterFeedCommandUp(shooterFeedSubsytem));
-    new JoystickButton(m_operatorController, Button.kX.value).whileHeld(new ShooterFeedCommandDown(shooterFeedSubsytem));
+    new JoystickButton(m_operatorController, Button.kX.value)
+        .whileHeld(new ShooterFeedCommandDown(shooterFeedSubsytem));
 
     new JoystickButton(m_operatorController, Button.kA.value).whileHeld(new ParallelCommandGroup(intakeCommandGroup()));
 
-    //new JoystickButton(m_operatorController, Button.kA.value).whenReleased(new ParallelCommandGroup(intakeStopCommandGroup()));
-    new JoystickButton(m_operatorController, Button.kLeftBumper.value).whenHeld(new StorageIntakeInCommand(storageIntake));
-    new JoystickButton(m_operatorController, Button.kRightBumper.value).whenHeld(new StorageIntakeOutCommand(storageIntake));
+    // new JoystickButton(m_operatorController, Button.kA.value).whenReleased(new
+    // ParallelCommandGroup(intakeStopCommandGroup()));
+    new JoystickButton(m_operatorController, Button.kLeftBumper.value)
+        .whenHeld(new StorageIntakeInCommand(storageIntake));
+    new JoystickButton(m_operatorController, Button.kRightBumper.value)
+        .whenHeld(new StorageIntakeOutCommand(storageIntake));
 
-    new JoystickButton(m_operatorController, Button.kB.value).whenHeld(new IntakeSensorsCommand(intakeSensors, shooterFeed, storageIntake, intakeMotor, intakeSolenoid));
+    new JoystickButton(m_operatorController, Button.kB.value)
+        .whenHeld(new IntakeSensorsCommand(intakeSensors, shooterFeed, storageIntake, intakeMotor, intakeSolenoid));
   }
 
-private void configureAutoChooser(){
-  m_chooser.addOption("MoveOneMeterAuto", moveOneMeter);
-  m_chooser.addOption("TwoPathsAuto", twoPaths);
-  m_chooser.addOption("IntakeRunAuto", intakeRun);
-  //m_chooser.addOption("ExampleAuto", exampleAuto);
-  Shuffleboard.getTab("Autonomous").add("SelectAuto", m_chooser).withSize(2, 1).withPosition(3, 1);
-  Shuffleboard.getTab("Autonomous").add("Documentation", "Autonomous Modes at https://stem2u.sharepoint.com/sites/frc-4329/_layouts/15/Doc.aspx?sourcedoc={91263377-8ca5-46e1-a764-b9456a3213cf}&action=edit&wd=target%28Creating%20an%20Autonomous%20With%20Pathplanner%7Cb37e1a20-51ec-9d4d-87f9-886aa67fcb57%2F%29").withPosition(2, 2).withSize(4, 1);
-}
+  private void configureAutoChooser() {
+    m_chooser.addOption("MoveOneMeterAuto", moveOneMeter);
+    m_chooser.addOption("TwoPathsAuto", twoPaths);
+    m_chooser.addOption("IntakeRunAuto", intakeRun);
+    // m_chooser.addOption("ExampleAuto", exampleAuto);
+    Shuffleboard.getTab("Autonomous").add("SelectAuto", m_chooser).withSize(2, 1).withPosition(3, 1);
+    Shuffleboard.getTab("Autonomous").add("Documentation",
+        "Autonomous Modes at https://stem2u.sharepoint.com/sites/frc-4329/_layouts/15/Doc.aspx?sourcedoc={91263377-8ca5-46e1-a764-b9456a3213cf}&action=edit&wd=target%28Creating%20an%20Autonomous%20With%20Pathplanner%7Cb37e1a20-51ec-9d4d-87f9-886aa67fcb57%2F%29")
+        .withPosition(2, 2).withSize(4, 1);
+  }
 
-  public Command getAuto(){
+  public Command getAuto() {
     return m_chooser.getSelected();
   }
 
