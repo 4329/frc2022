c918eeaaa5708cac65439eb6431025cb9fae19b9
Harmiess <alexanderstamper@gmail.com>
2022-02-15 18:30:26 -0600

    added command, missing button

---
+++ src/main/java/frc/robot/Commands/shooterAutoFireCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Subsystems.Shooter;
+import frc.robot.Subsystems.ShooterFeedSubsytem;
+import frc.robot.Subsystems.StorageIntake;
+
+public class shooterAutoFireCommand {
+    private ShooterFeedSubsytem shooterFeed;
+    private StorageIntake storageIntake;
+    private Shooter shooter;
+
+    public shooterAutoFireCommand(ShooterFeedSubsytem shooterFeed, StorageIntake storageIntake, Shooter shooter) {
+        this.shooterFeed = shooterFeed;
+        this.storageIntake = storageIntake;
+        this.shooter = shooter;
+    }
+
+    public void initialize() {
+        shooterFeed.shooterFeedUp();
+        storageIntake.storageIntakeIn();
+        shooter.shoot(0.5);//don't know what this will do
+    }
+
+    public void end() {
+        shooterFeed.shooterFeedUp();
+        storageIntake.storageIntakeIn();
+        shooter.holdFire();
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+}
