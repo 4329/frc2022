7d0512b8010544dc30f092fffbc5f846c396609b
Harmiess <alexanderstamper@gmail.com>
2022-02-15 19:44:29 -0600

    rewrote command

--- src/main/java/frc/robot/Commands/SensorOutputCommand.java
+++ src/main/java/frc/robot/Commands/SensorOutputCommand.java
@@ -1,10 +1,17 @@
 package frc.robot.Commands;
 
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Subsystems.IntakeSensors;
 
-public class SensorOutput extends StartEndCommand {
-    public SensorOutput(IntakeSensors subsystem) {
-        super(subsystem::topTrigger, subsystem::bottomTrigger);
+public class SensorOutputCommand extends CommandBase {
+    private IntakeSensors intakeSensors;
+
+    public SensorOutputCommand(IntakeSensors intakeSensors) {
+        this.intakeSensors = intakeSensors;
+    }
+
+    public void execute() {
+        intakeSensors.topTrigger();
+        intakeSensors.bottomTrigger();
     }
 }
