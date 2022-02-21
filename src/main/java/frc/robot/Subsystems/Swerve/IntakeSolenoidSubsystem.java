5fa48437a9c0e470661fd014d89250ebae04af52
David <you@example.com>
2022-02-21 13:52:05 -0600

    committing to switch branches

--- src/main/java/frc/robot/Subsystems/Swerve/IntakeSolenoidSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Swerve/IntakeSolenoidSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.Subsystems.Swerve;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configrun;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
 
 //import com.fasterxml.jackson.databind.node.NullNode;
 
@@ -15,17 +17,31 @@ import edu.wpi.first.wpilibj.Solenoid;
 public class IntakeSolenoidSubsystem extends SubsystemBase {
     
     
-    private static final int SOLENOID_CHANNEL = 0;
+    //private static final int SOLENOID_CHANNEL = 0;
     private Solenoid m_Solenoid;
-    
+    private DoubleSolenoid m_doubleSolenoid = null;
+
     boolean intakeUp;
 
     public IntakeSolenoidSubsystem(PneumaticHub pneumaticHub) {
 
         intakeUp = Configrun.get(false, "intakeUp");
-        m_Solenoid = pneumaticHub.makeSolenoid(SOLENOID_CHANNEL);
-        m_Solenoid.set(Configrun.get(true, "intakeUp"));
+        //m_Solenoid = pneumaticHub.makeSolenoid(SOLENOID_CHANNEL);
+        //m_Solenoid.set(Configrun.get(true, "intakeUp"));
+        m_doubleSolenoid = pneumaticHub.makeDoubleSolenoid(14, 15);
     }
+
+    public void Forward() {
+        System.out.println("going forward");
+        m_doubleSolenoid.set(kForward);
+       
+      }
+    
+      public void Reverse() {
+        System.out.println("going reverse");
+        m_doubleSolenoid.set(kReverse);
+        
+      }
 
     public void intakeUp() {
 
@@ -58,5 +74,28 @@ public class IntakeSolenoidSubsystem extends SubsystemBase {
 
         m_Solenoid.toggle();
     }
+ 
+    
+    
+    //private static final int SOLENOID_CHANNEL = 0;
+
+  
+
+    //private Solenoid m_Solenoid = null;
+
+    //boolean intakeUp;
+
+    
+
+    public IntakeSolenoidSubsystem(PneumaticHub pneumaticHub) {
+        /*intakeUp = Configrun.get(true, "intakeUp");
+        // changed from intake = Configrun.get(false, "intakeUp")
+        // also switcehd the hoses or whatever its called on the solenoids
+        System.out.println("going to make new solenoid");
+        m_Solenoid = pneumaticHub.makeSolenoid(SOLENOID_CHANNEL);
+        System.out.println("i made it"); */
+        
+    }
+
     
 }
