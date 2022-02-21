0b06501210df4f276ba133d7977ada1cf1e711de
David <you@example.com>
2022-02-21 14:19:24 -0600

    intake double solenoids now toggle and are seprate from the intake motor

--- src/main/java/frc/robot/Subsystems/Swerve/IntakeSolenoidSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Swerve/IntakeSolenoidSubsystem.java
@@ -3,6 +3,8 @@ package frc.robot.Subsystems.Swerve;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configrun;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kForward;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kReverse;
 
 
 //import com.fasterxml.jackson.databind.node.NullNode;
@@ -18,7 +20,7 @@ public class IntakeSolenoidSubsystem extends SubsystemBase {
     
     
     //private static final int SOLENOID_CHANNEL = 0;
-    private Solenoid m_Solenoid;
+    //private Solenoid m_Solenoid;
     private DoubleSolenoid m_doubleSolenoid = null;
 
     boolean intakeUp;
@@ -31,28 +33,18 @@ public class IntakeSolenoidSubsystem extends SubsystemBase {
         m_doubleSolenoid = pneumaticHub.makeDoubleSolenoid(14, 15);
     }
 
-    public void Forward() {
-        System.out.println("going forward");
-        m_doubleSolenoid.set(kForward);
-       
-      }
-    
-      public void Reverse() {
-        System.out.println("going reverse");
-        m_doubleSolenoid.set(kReverse);
-        
-      }
-
     public void intakeUp() {
 
         intakeUp = true;
-        m_Solenoid.set(true);
+        //m_Solenoid.set(true);
+        m_doubleSolenoid.set(kReverse);
         System.out.println("intakeup");
     }
 
     public void intakeDown() {
 
-        m_Solenoid.set(false);
+        //m_Solenoid.set(false);
+        m_doubleSolenoid.set(kForward);
         intakeUp = false;
         System.out.println("intakedown");
     }
@@ -61,18 +53,20 @@ public class IntakeSolenoidSubsystem extends SubsystemBase {
 
         if (intakeUp) {
 
-            m_Solenoid.set(false);
+            //m_Solenoid.set(false);
+            m_doubleSolenoid.set(kForward);
             intakeUp = false;
         } else {
 
-            m_Solenoid.set(true);
+            //m_Solenoid.set(true);
+            m_doubleSolenoid.set(kReverse);
             intakeUp = true;
         }
     }
 
     public void toggleIntake() {
 
-        m_Solenoid.toggle();
+        m_doubleSolenoid.toggle();
     }
  
     
@@ -84,18 +78,6 @@ public class IntakeSolenoidSubsystem extends SubsystemBase {
     //private Solenoid m_Solenoid = null;
 
     //boolean intakeUp;
-
-    
-
-    public IntakeSolenoidSubsystem(PneumaticHub pneumaticHub) {
-        /*intakeUp = Configrun.get(true, "intakeUp");
-        // changed from intake = Configrun.get(false, "intakeUp")
-        // also switcehd the hoses or whatever its called on the solenoids
-        System.out.println("going to make new solenoid");
-        m_Solenoid = pneumaticHub.makeSolenoid(SOLENOID_CHANNEL);
-        System.out.println("i made it"); */
-        
-    }
 
     
 }
