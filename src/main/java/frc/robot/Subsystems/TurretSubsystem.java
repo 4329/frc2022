a20b0cba6052a90f30b8db37e025e1e5a1e5d268
Harmiess <alexanderstamper@gmail.com>
2022-02-26 17:03:21 -0600

    Limlight works, with cleanup

--- src/main/java/frc/robot/Subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/Subsystems/TurretSubsystem.java
@@ -1,32 +1,23 @@
 package frc.robot.Subsystems;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-import frc.robot.Configrun;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import frc.robot.PIDConstants;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configrun;
 
 public class TurretSubsystem extends SubsystemBase{
 
+    private static final double LIMLIGHT_RANGE = 30;
     private TalonSRX turret;
-    private double output, error;
-
     private volatile int lastValue = Integer.MIN_VALUE;
 
-    PIDController pidController;
     double staticFeedforward = 0;
-
-    SmartDashboard table;
-    SmartDashboard ledSmartDashboard;
     // default value for the limelight mode
     int defaultvalue = 1;
     PIDController limeLightPid;
@@ -37,11 +28,9 @@ public class TurretSubsystem extends SubsystemBase{
     double a1Degree = Configrun.get(0.0, "a1Degree");
     // angle of the front of the limelight in relation to level
     double limeLightDistance;
-    int limeLightTolerance = Configrun.get(1, "limeLightTolerance");
-    double limelightP = Configrun.get(1.2, "limelightP");
-    double limelightI = Configrun.get(0, "limelightI");
-    double limelightD = Configrun.get(0.125, "limelightD");
-    double taTolerance;
+    int limeLightTolerance = 1;
+    double taTolerance = 0.3;
+
     public double currentDistance = 120;
 
     private NetworkTableEntry checkTXDisplay;
@@ -50,31 +39,16 @@ public class TurretSubsystem extends SubsystemBase{
     private NetworkTableEntry getDistanceFromTargetDisplay;
     NetworkTableEntry targetStatus;
 
-
     public TurretSubsystem() {
         turret = new TalonSRX (Configrun.get(41, "turretID"));
-        turret.getSensorCollection();
-        // limeLightPid = new PIDController(limelightP, limelightI, limelightD);
         limeLightPid = new PIDController(1, 0, 0);
-        taTolerance = Configrun.get(0.3, "taTolerance");
         limeLightPid.setTolerance(limeLightTolerance);
         targetStatus = Shuffleboard.getTab("RobotData").add("Target Acquired", false).getEntry();
-        checkTXDisplay = Shuffleboard.getTab("limlight").add("Tx", 0).withPosition(3, 1).getEntry();
-        checkTYDisplay = Shuffleboard.getTab("limlight").add("TY", 0).withPosition(3, 0).getEntry();
-        checkTADisplay = Shuffleboard.getTab("limlight").add("TA", 0).withPosition(4, 0).getEntry();
-        getDistanceFromTargetDisplay = Shuffleboard.getTab("limlight").add("Distance", 0).withPosition(5, 0).getEntry();
-        pidController = new PIDController(PIDConstants.ROTATION_P, PIDConstants.ROTATION_I, PIDConstants.ROTATION_D);
-        // pidController.setInputRange(0.0, 360.0);
-        // pidController.setContinuous(true);
-        // pidController.setOutputRange(-1.0, 1.0);
-        pidController.setTolerance(PIDConstants.TOLERANCE);
-        //pidController.enableContinuousInput(0, 360);
+        checkTXDisplay = Shuffleboard.getTab("Limlight").add("Tx", 0).withPosition(3, 1).getEntry();
+        checkTYDisplay = Shuffleboard.getTab("Limlight").add("TY", 0).withPosition(3, 0).getEntry();
+        checkTADisplay = Shuffleboard.getTab("Limlight").add("TA", 0).withPosition(4, 0).getEntry();
+        getDistanceFromTargetDisplay = Shuffleboard.getTab("Limlight").add("Distance", 0).withPosition(5, 0).getEntry();
     }
-
-    // public void setDistance() {
-    //     currentDistance = getDistanceFromTarget();
-    // }
-    // Sets calculation distance for shooter
 
     public void putValuesToShuffleboard() {
         checkTXDisplay.setDouble(NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0));
@@ -83,7 +57,7 @@ public class TurretSubsystem extends SubsystemBase{
         getDistanceFromTargetDisplay.setDouble(getDistanceFromTarget());
     }
 
-    public double checkTx() {
+    public double getTx() {
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
         NetworkTableEntry tx = table.getEntry("tx");
         double x = tx.getDouble(0.0);
@@ -91,6 +65,7 @@ public class TurretSubsystem extends SubsystemBase{
     }
 
     public boolean targetVisible() {
+        // check Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
         NetworkTableEntry tv = table.getEntry("tv");
         double v = tv.getDouble(0.0);
@@ -100,33 +75,32 @@ public class TurretSubsystem extends SubsystemBase{
         }
         return false;
     }
-    // check Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
 
-    public double checkTa() {
+    public double getTa() {
+        // checks the area visible of the Target (0% of image to 100% of image)
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
         NetworkTableEntry ta = table.getEntry("ta");
         double a = ta.getDouble(0.0);
         return a;
     }
-    // checks the area visible of the Target (0% of image to 100% of image)
 
-    public double checkTy() {
+    public double getTy() {
+        // checks Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
         NetworkTableEntry ty = table.getEntry("ty");
         double y = ty.getDouble(0.0);
         return y;
     }
-    // checks Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
 
     public double getDistanceFromTarget() {
-        limeLightDistance = (h2In - h1In) / Math.tan(Math.toRadians(a1Degree) + (Math.toRadians(checkTy())));
+        // TODO use this to get distance from target (only while target is visible)
+        limeLightDistance = (h2In - h1In) / Math.tan(Math.toRadians(a1Degree) + (Math.toRadians(getTy())));
         return limeLightDistance;
     }
 
     public void limeLightOn() {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").setNumber(defaultvalue);
         // turns the limelight on using defaulvalue
-
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").setNumber(defaultvalue);
     }
 
     public void limeLightStop() {
@@ -136,12 +110,10 @@ public class TurretSubsystem extends SubsystemBase{
     public void putTargetAcquired() {
         boolean status;
         // true or false value
-        if (checkTa() >= taTolerance) {
+        if (getTa() >= taTolerance) {
             status = true;
-            // System.out.println("limelighttrue");
         } else {
             status = false;
-            // System.out.println("limelightfalse");
         }
         targetStatus.setBoolean(status);
     }
@@ -174,21 +146,9 @@ public class TurretSubsystem extends SubsystemBase{
 
         if(targetVisible()) {
             if(getPwmPosition() >= Configrun.get(943, "turretMin") && output > 0) {
-                /*if (Math.abs(output) >= 0.2 && Math.abs(output) <= 0.5)*{
-                turretPower(output);
-                }
-                else {
-                    turretStop();
-                } */
                 turretPower(output);
             }
             else if(getPwmPosition() <= Configrun.get(1557, "turretMax") && output < 0) {
-                // if (Math.abs(output) >= 0.2 && Math.abs(output) <= 0.5){
-                //   turretPower(output);
-                // }
-                // else {
-                //     turretStop();
-                // }
                 turretPower(output);
             }
             else {
@@ -201,45 +161,17 @@ public class TurretSubsystem extends SubsystemBase{
     }
 
     public void targeting() {
-            double output = limeLightPid.calculate(checkTx(), 0);
-            //30 is the limiting factor for the output
-            output = output / 30;
-            if (output < 0) {
-                output = output - staticFeedforward;
-            }
-            else {
-                output = output + staticFeedforward;
-            }
+        double output = limeLightPid.calculate(getTx(), 0);
+        //converts range to % power
+        output = output / LIMLIGHT_RANGE;
+        if (output < 0) {
+            output = output - staticFeedforward;
+        }
+        else {
+            output = output + staticFeedforward;
+        }
 
-            rotateTurret(output);
-            // setDistance();
-            putValuesToShuffleboard();
+        rotateTurret(output);
+        putValuesToShuffleboard();
     }
-
-
-
-
-    // Runs the PID loop for angles within delta 90
-    // public double controlRotationWithin90(double x, SwerveModule module) {
-    //     // The wheel is commanded directly to the requested angle
-    //     pidController.setSetpoint(x);
-    //     output = pidController.calculate(.getAngle());
-    //     output = output / 90;
-    //     // SmartDashboard.putNumber("Output" + module.getPotentiometerPort(), output);
-    //     error = pidController.getPositionError();
-    //     // SmartDashboard.putNumber("Error" + module.getPotentiometerPort(), error);
-    //     return output;
-    // }
-
-    public void turretRight(){
-        turret.set(TalonSRXControlMode.PercentOutput, -Configrun.get(0.5, "turretRightPower"));
-    }
-
-
-
-    // public void turretPeriodic(){
-    //     double output = hoodPID.calculate(hoodposition, hoodSetpoint.getDouble(0));
-    // output = output / MAX_RANGE;
-    // setpointDifference = hoodSetpoint.getDouble(0) - hoodposition;
-    // }
 }
