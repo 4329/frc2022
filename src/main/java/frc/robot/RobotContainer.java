813b8a55205cbdd44ff23aac8cff8cc68d0657dc
Khai_Wall <Khai.wall@stem2u.org>
2022-03-05 14:59:35 -0600

    when robot enable, arms should retract and 'reversePivot' this is to be tested

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -231,6 +231,9 @@ public class RobotContainer {
 
     turretSubsystem.setDefaultCommand(turretToZeroCommand);
     climber.engage();
+    climber.retract();
+    climber.reversePivotClimber();
+
   }
 
   public void teleopPeriodic() {
